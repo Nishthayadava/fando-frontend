733d74ab1586ed30253b41fd189cec131097ed12
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-07 01:24:26 +0530

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,16 +4,33 @@ import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
+    const [open, setOpen] = React.useState(true); // Default to open
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+      };
+    
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap>
-          Welcome To F&O Expert CRM
-        </Typography>
-      </Toolbar>
+
+<Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                mr: 2,
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Welcome To F&O Expert CRM
+            </Typography>
+          </Toolbar>
+     
     </AppBar>
   );
 };
