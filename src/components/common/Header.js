6d399536f06ae19a2a89ff84a8c5e0751ab7a6de
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-06 23:33:47 +0530

    Create Header.js

---
+++ src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Header = () => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6  noWrap component">Welcome To F&O Expert CRM</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
