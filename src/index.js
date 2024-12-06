c8a00341b7a35e8cee4e832966c987089609368a
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-07 02:14:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+// Use createRoot instead of render
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
