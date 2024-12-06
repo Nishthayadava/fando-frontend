8e5bebff92dcc5ba44288768c069dd6ec2c4f947
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-07 00:07:52 +0530

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,3 +1,4 @@
+// Header.js
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -9,7 +10,9 @@ const Header = () => {
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6  noWrap component">Welcome To F&O Expert CRM</Typography>
+        <Typography variant="h6" noWrap>
+          Welcome To F&O Expert CRM
+        </Typography>
       </Toolbar>
     </AppBar>
   );
