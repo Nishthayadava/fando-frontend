31378b0af86a370271a068b6dd6656c94fc18ea7
Nishtha Yadav <95894895+Nishthayadava@users.noreply.github.com>
2024-12-06 23:43:33 +0530

    Create AuthenticatedRoute.js

---
+++ src/components/common/AuthenticatedRoute.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+const AuthenticatedRoute = ({ element, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem('token');
+  return (
+    <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<div>Error Page 404  Not found</div>} />
+
+            <Route
+              path="/dashboard"
+              element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/OverallSales"
+              element={isAuthenticated() ? <OverallSales /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/salesAgentwise"
+              element={isAuthenticated() ? <SalesAgentwise /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/tablePage"
+              element={isAuthenticated() ? <TablePage /> : <Navigate to="/login" />}
+            />
+
+              <Route
+                        path="/followup"
+                        element={isAuthenticated() ? <FollowUp /> : <Navigate to="/login" />}
+                      />
+
+            <Route
+              path="/salesAgentwise"
+              element={isAuthenticated() ? <QualityAndCompliance /> : <Navigate to="/login" />}
+            />
+
+
+            <Route
+              path="/admindashboard"
+              element={isAuthenticated() ? <AdminDashboard /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/create-user"
+              element={isAuthenticated()? <CreateUser /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/upload-leads"
+              element={isAuthenticated() ? <UploadLeads /> : <Navigate to="/login" />}
+            />
+
+            <Route
+              path="/leads"
+              element={isAuthenticated() ? <LeadsDashboard /> : <Navigate to="/login" />}
+            />
+
+
+            <Route
+              path="/paidCustomer"
+              element={isAuthenticated() ? <PaidCustomer /> : <Navigate to="/login" />}
+            />
+
+      
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+  );
+};
+
+export default AuthenticatedRoute;
